8082d3ceaebe06f1459a941159ef024ab89c47d2
Muhammad Haseeb Asif <haseeb1431@gmail.com>
2015-06-07 20:28:38 +0500

    First commit

---
+++ WcfService/IService1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IGreenPill
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        System.Guid WhatIsYourToken();
+
+        [OperationContract]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentOutOfRangeException), Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberArgumentOutOfRangeException" +
+            "Fault", Name = "ArgumentOutOfRangeException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        long FibonacciNumber(long n);
+
+        [OperationContract]
+        WcfService.RedPill.TriangleType WhatShapeIsThis(int a, int b, int c);
+
+        [OperationContract]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentNullException), Name = "ArgumentNullException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        string ReverseWords(string s);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfService/Service1.svc.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfService.RedPill;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class GreenPill : IGreenPill
+    {
+        RedPillClient redpillClient;
+        public GreenPill()
+        {
+            redpillClient = new RedPillClient("BasicHttpsBinding_IRedPill");
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+
+        public Guid WhatIsYourToken()
+        {
+            return redpillClient.WhatIsYourToken();
+        }
+
+        public System.Threading.Tasks.Task<Guid> WhatIsYourTokenAsync()
+        {
+            return redpillClient.WhatIsYourTokenAsync();
+        }
+
+        public long FibonacciNumber(long n)
+        {
+            return redpillClient.FibonacciNumber(n);            
+        }
+
+        public System.Threading.Tasks.Task<long> FibonacciNumberAsync(long n)
+        {
+            return redpillClient.FibonacciNumberAsync(n);
+        }
+
+        public RedPill.TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            return redpillClient.WhatShapeIsThis(a, b, c);
+        }
+
+        public System.Threading.Tasks.Task<RedPill.TriangleType> WhatShapeIsThisAsync(int a, int b, int c)
+        {
+            return redpillClient.WhatShapeIsThisAsync(a, b, c);
+        }
+
+        public string ReverseWords(string s)
+        {
+            return redpillClient.ReverseWords(s);
+        }
+
+        public System.Threading.Tasks.Task<string> ReverseWordsAsync(string s)
+        {
+            return redpillClient.ReverseWordsAsync(s);
+        }
+    }
+}
